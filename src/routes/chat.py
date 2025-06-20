46875be5539b06dad376e796d98433ffa6739616
Brian Clements <brian.l.clements03@gmail.com>
2025-06-20 10:31:15 -0500

    update chat.py

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -3,9 +3,12 @@ from fastapi import APIRouter, FastAPI, WebSocket, Request, Depends, HTTPExcepti
 import uuid
 from src.socket.connection import ConnectionManager
 from src.socket.utils import get_token
+from server.src.redis.producer import Producer
+from server.src.redis.config import Redis
 
 chat = APIRouter()
 manager = ConnectionManager()
+redis = Redis()
 
 # @route    POST /token
 # @desc     Route to generate chat token
@@ -38,10 +41,16 @@ async def refresh_token(request: Request):
 @chat.websocket("/chat")
 async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_token)):
     await manager.connect(websocket)
+    redis_client = await redis.create_connection()
+    producer = Producer(redis_client)
+
     try:
         while True:
             data = await websocket.receive_text()
             print(data)
+            stream_data = {}
+            stream_data[token] = data
+            await producer.add_to_stream(stream_data, "message_channel")
             await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
 
     except WebSocketDisconnect:
